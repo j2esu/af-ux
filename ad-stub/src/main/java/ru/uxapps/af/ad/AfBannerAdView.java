f4057c3bbbe7ea1f16af30c2bec4f35dcf9482bd
Evgeny Egorov <ipegorovev@gmail.com>
2018-02-25 13:27:26 +0300

    Add to Bintray.

--- ad-stub/src/main/java/ru/uxapps/af/ad/AfBannerAdView.java
+++ ad-stub/src/main/java/ru/uxapps/af/ad/AfBannerAdView.java
@@ -1,18 +1,16 @@
 package ru.uxapps.af.ad;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
 public class AfBannerAdView extends FrameLayout {
 
-    public AfBannerAdView(@NonNull Context context) {
+    public AfBannerAdView(Context context) {
         super(context);
     }
 
-    public AfBannerAdView(@NonNull Context context, @Nullable AttributeSet attrs) {
+    public AfBannerAdView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 }
